6656aed50c20c204d1c9a1716effa5eef0b8a50f
ExiledRain <gett_lostt@bk.ru>
2021-03-29 15:31:58 +0300

    EB-0 Spring boot base for the project

---
+++ src/main/java/io/exiled/employeebooking/EmployeebookingApplication.java
@@ -0,0 +1,13 @@
+package io.exiled.employeebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeebookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeebookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/exiled/employeebooking/EmployeebookingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.exiled.employeebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeebookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
